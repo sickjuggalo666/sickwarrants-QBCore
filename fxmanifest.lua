16a5b0bab16bf2d7d8d235f06676becfaf853e2b
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2022-11-26 12:50:45 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,30 @@
 
+fx_version 'bodacious'
+
+games { 'rdr3', 'gta5' }
+
+lua54 'yes'
+
+mod 'sickwarrants-qb'
+
+version '1.0.0'
+
+shared_scripts {
+
+    'config.lua'
+
+}
+
+client_scripts {
+
+    'client.lua',
+
+}
+
+server_scripts {
+
+    '@mysql-async/lib/MySQL.lua',
+
+    'server.lua',
+
+}
